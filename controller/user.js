771fb1068e59ce61f304dd873e6e040ec6bea977
paizal perlando <paizalperlando25092000@gmail.com>
2021-04-27 04:10:26 +0700

    pertemuan_8

--- controller/user.js
+++ controller/user.js
@@ -1,36 +1,65 @@
-const user = require('../model/user')
-const userModel = require('../model/user')
+const  userModel = require('../model/User')
 const bcrypt = require('bcrypt')
 
-exports.register = (data) => 
-    new Promise((resolve, reject) => {
-        userModel.findOne({
-            username: data.username
-        }).then (adaUser => {
-        if (adaUser) {    
+exports.register = (data) =>
+new Promise((resolve, reject) => {
+    //console.log(data)
+    userModel.findOne({
+        username: data.username
+    }).then(adaUser => {
+        if (adaUser) {
             resolve({
                 status: false,
-                pesan: 'username telah terdaftar'
-                })
-             } else {
-               bcrypt.hash(data.password, 10, (err, hash) => {
+                pesan: 'user telah terdaftar'
+            })
+        } else {
+            bcrypt.hash(data.password, 10, (err, hash) => {
                 data.password = hash
                 userModel.create(data)
-                        .then(() => {
-                            //console.log('berhasil insert')
-                            resolve({
-                                status: true,
-                                pesan: 'berhasil insert data user'
-                            })
-                        }).catch((e) => {
-                            console.log('gagal insert')
-                            reject({
-                            status: false,
-                            pesan: 'gagal insert data user'
-                            })
-                        }) 
-               })  
-                //console.log('belum ada username')  
-            } 
+                .then(() => {
+                    //console.log('berhasil Insert')
+                    resolve({
+                        status: true,
+                        pesan: 'berhasil insert data user'
+                    })
+                }).catch(() => {
+                    //console.log('gagal Insert')
+                    reject({
+                        status: false,
+                        pesan: 'gagal insert data user'
+                    })
+                })
+            })
+        }
+    })
+})
+
+exports.login = (data) =>
+new Promise((resolve, reject) =>{
+    try {
+        userModel.findOne({
+            username: data.username
+        }).then(user => {
+            if (user) {
+                if (bcrypt.compareSync(data.password, user.password)) {
+                    resolve({
+                        status: true,
+                        pesan: 'Berhasil Login'
+                    })
+                } else {
+                    reject({
+                        status: false,
+                        pesan: 'Password Tidak Sesuai'
+                    })
+                }
+            } else {
+                reject({
+                    status: false,
+                    pesan: 'Username Tidak terdaftar'
+                })
+            }
         })
+    } catch (e) {
+        console.log(e)
+    }
 })
